b2d0cf2832f9e01e09534a5bac5fe4234e218687
HARIHARAN R MOHAN <harryharan98@gmail.com>
2022-04-25 15:31:27 +0800

    Update Connection

--- connection.php
+++ connection.php
@@ -14,19 +14,19 @@
 // }
 
 /* Database credentials. Assuming you are running MySQL server with default setting (user 'root' with no password) */
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'tfos';
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $db_name = 'tfos';
 
-/* Establishes the connection */
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+// /* Establishes the connection */
+// $conn = mysqli_init();
+// mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 
-/* Check connection */
-if (mysqli_connect_errno($conn)) {
-	die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+// /* Check connection */
+// if (mysqli_connect_errno($conn)) {
+// 	die('Failed to connect to MySQL: '.mysqli_connect_error());
+// }
 
 
 // $host = 'z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
@@ -43,18 +43,18 @@ if (mysqli_connect_errno($conn)) {
 // }
 
 
-// /* Database credentials. Assuming you are running MySQL
-// server with default setting (user 'root' with no password) */
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_NAME', 'testtfsystem');
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tfos');
  
-// /* Attempt to connect to MySQL database */
-// $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+/* Attempt to connect to MySQL database */
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// // Check connection
-// if($mysqli === false){
-//     die("ERROR: Could not connect. " . mysqli_connect_error());
-// }
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
